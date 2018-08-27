ca770abbbcbe5598586fc8eb57619ee110104963
ziyinz-XYZ <39889615+ziyinz-XYZ@users.noreply.github.com>
2018-08-27 17:39:03 -0400

    Update Obj.cpp

--- Obj.cpp
+++ Obj.cpp
@@ -1,3 +1,10 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 5, 2018
+ *      Author: ZING
+ */
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -37,6 +44,11 @@ public:
 	}
 };
 
+Obj func0() {
+	Obj obj;
+	return obj;
+}
+
 Obj func1(Obj obj) {
 	cout << "func1 begins" << endl;
 	cout << "ready to return" << endl;
@@ -54,12 +66,16 @@ Obj func3(Obj&& obj) {
 	cout << "ready to return" << endl;
 	return obj;
 }
-
+//-fno-elide-constructors
 
 int main() {
 	Obj obj(1); // normal constructor
 	Obj obj1 = obj; // copy operator =
 	Obj obj2; // normal constructor
+
+	cout << "######test for func0" << endl;
+	obj2 = func0();
+
 	cout << "######test for func1" << endl;
 	obj2 = func1(obj);
 
@@ -70,5 +86,6 @@ int main() {
 	// obj2 = func3(obj); // compile error
 	obj2 = func3(func1(obj));
 
+	cout << "ready to return from main" << endl;
 	return 0;
 }
